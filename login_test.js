9372099d645ae71ac76565afb748a12c07d1fb8e
João Paulo <seuemail@exemplo.com>
2025-06-20 22:49:08 -0300

    adicionando testes

--- login_test.js
+++ login_test.js
@@ -4,23 +4,45 @@ Feature('login');
 Scenario('Login com sucesso',  ({ I }) => {
    
    I.amOnPage('https://www.automationpratice.com.br/')
-})
+   I.click('Login')
+   I.waitForText('Login',10)
+   I.fillField("#user", 'joao.ppaulo92@hotmail.com')
+   I.fillField("#password", '123456')
+   I.click('#btnLogin')
+   I.waitForText('Login realizado',5)
+}).tag('loginComSucesso')
 
 
-S
-cenario('Tentando Logar digitando apenas o e-mail',  ({ I }) => {
+Scenario('Tentando Logar digitando apenas o e-mail',  ({ I }) => {
    
    I.amOnPage('https://www.automationpratice.com.br/')
-});
+   I.click('Login')
+   I.waitForText('Login',10)
+   I.fillField("#user", 'joao.ppaulo92@hotmail.com')
+   I.click('#btnLogin')
+   I.waitForText('Senha inválida.',5)
+}).tag('apenasEmail')
 
 
 Scenario('Tentando logar sem digitar e-mail e senha',  ({ I }) => {
    
    I.amOnPage('https://www.automationpratice.com.br/')
-});
+   I.click('Login')
+   I.waitForText('Login',10)
+   I.click('#btnLogin')
+   I.waitForText('E-mail inválido.',5)
+ 
+}).tag('semEmaileSenha')
+
 
 
 Scenario('Tentando Logar digitando apenas a senha',  ({ I }) => {
    
    I.amOnPage('https://www.automationpratice.com.br/')
-});
+   I.click('Login')
+   I.waitForText('Login',10)
+   I.click('#btnLogin')
+   I.fillField('#password','123456')
+   I.waitForText('E-mail inválido.',5)
+ 
+}).tag('apenasSenha')
